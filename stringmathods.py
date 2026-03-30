f693864770ba14cb6e6ed5be6d30322d8b5a0d3c
Devisriraghavulu <srisaibalaji243@gmail.com>
2026-03-30 15:18:16 +0530

    Add files via upload

---
+++ stringmathods.py
@@ -0,0 +1,13 @@
+a="hai"
+b="halo"
+c="cse3"
+d="i like java"
+print(a.islower())
+print(b.isupper())
+print(b.isdigit())
+print(a.isalpha())
+print(a.upper())
+print(b.islower())
+print(c.isalnum())
+print(d.replace("java","python"))
+print(d.find("like"))
