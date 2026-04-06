108027e509a51a776292390d5d41c79426715b9f
Devisriraghavulu <srisaibalaji243@gmail.com>
2026-04-06 15:39:47 +0530

    Add files via upload

---
+++ sec into min.py
@@ -0,0 +1,15 @@
+class Time:
+    def read(self,seconds):
+        self.seconds=seconds
+    def to_convert(self):
+        self.minutes=self.seconds//60
+        self.hours=self.seconds//3600
+        self.day=self.seconds//86400
+    def display(self):
+        print("hours:",self.hours)  
+        print("minutes:",self.minutes)  
+        print("days:",self.day)    
+t=Time()
+t.read(86400)
+t.to_convert()
+t.display()
